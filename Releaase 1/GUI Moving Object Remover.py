6c4f545948ad559f9e195d2d18217fd9ff04339d
Rasika <rasikat77@gmail.com>
2019-01-23 00:15:44 +0530

    Added stabilizing parts

--- Releaase 1/GUI Moving Object Remover.py
+++ Releaase 1/GUI Moving Object Remover.py
@@ -1,31 +1,39 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
-
-
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk,GdkPixbuf
 from  mor import MovingObjectRemover
 
 class ButtonWindow(Gtk.Window):
     remover = MovingObjectRemover()
     img=None
+    img_computed=None
+    image = Gtk.Image()
     def __init__(self):
         Gtk.Window.__init__(self, title="Moving Object Remover")
         self.set_border_width(10)
-
-        hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
+        #self.set_default_size (500,300)
+        grid = Gtk.Grid()
+        self.add(grid)
 
         button = Gtk.Button.new_with_mnemonic("_Open")
         button.connect("clicked", self.on_open_clicked)
-        hbox.pack_start(button, True, True, 0)
+        grid.attach(button,0, 0, 1, 1)
+        
+        label = Gtk.Label("                                                                             ")
+        grid.attach(label,2, 0, 6, 1)
 
         button = Gtk.Button.new_with_mnemonic("_Save as Jpg")
         button.connect("clicked", self.on_save_clicked)
-        hbox.pack_start(button, True, True, 0)
+        grid.attach(button,8, 0, 1, 1)
 
+        
+        #button.connect("clicked", self.on_save_clicked)
+        self.image.set_from_file('./tmp.jpg')
+        grid.attach(self.image,0, 2, 9, 1)
+        label = Gtk.Label("                                                                             ")
+        grid.attach(label,2, 1, 6, 1)
+
+        self.set_resizable(False)
     def on_click_me_clicked(self, button):
         print("\"Click me\" button was clicked")
 
@@ -41,6 +49,17 @@ class ButtonWindow(Gtk.Window):
             print("Folder selected: " + dialog.get_filename())
             ##
             self.img=self.remover.open_image_set(dialog.get_filename())
+            self.img_computed=self.remover.compute_and_save(self.img,'./tmp.jpg')
+            pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
+                filename="./tmp.jpg", 
+                width=24, 
+                height=24, 
+                preserve_aspect_ratio=True)
+
+            self.image = Gtk.Image.new_from_pixbuf(pixbuf)
+            #self.image.set_from_file('./tmp.jpg')
+            dialog.set_default_size(100, 100)
+
             #
             ##
         elif response == Gtk.ResponseType.CANCEL:
@@ -62,7 +81,7 @@ class ButtonWindow(Gtk.Window):
             print("Open clicked")
             print("File selected: " + dialog.get_filename())
             #
-            self.remover.compute_and_save(self.img,dialog.get_filename())
+            self.remover.save_image(self.img_computed,dialog.get_filename())
             #
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
--- Releaase 1/mor.py
+++ Releaase 1/mor.py
@@ -24,4 +24,72 @@ class MovingObjectRemover:
         return images
         
     def compute_and_save(self,img,path='./tmp.jpg'):
-        self.save_image(np.median(img,axis=0),path)
+        img=np.median(img,axis=0)
+        self.save_image(img,path)
+        return img
+    
+    #def save_image(self,img,path='./tmp.jpg'):
+
+
+    def genarate_bound(self,img,x_h,x_l,y_h,y_l):
+        zr_y_l=np.zeros((y_l,img.shape[1],img.shape[2]))
+        zr_y_h=np.zeros((y_h,img.shape[1],img.shape[2]))
+        #print ( zr[0].size)
+        #print(zr.shape)
+        #print(img.shape)
+        resized=np.concatenate((zr_y_l,img,zr_y_h),axis=0)
+        #print(resized.shape)
+        #print(img.shape[1])
+        zr_x_l=np.zeros((resized.shape[0],x_l,resized.shape[2]))
+        zr_x_h=np.zeros((resized.shape[0],x_h,resized.shape[2]))
+        resized=np.concatenate((zr_x_l,resized,zr_x_h),axis=1)
+        return resized
+    
+    def bound_for_each(self,imgs,bound_x,bound_y):
+        ret = []
+        for img in imgs:
+            ret.append(self.genarate_bound(img,bound_x,bound_x,bound_y,bound_y))
+        return ret
+
+    def get_best_match(self,im_base,im_second,bound=10):
+        base_d=self.gradiant(im_base,5)
+        base_image=self.genarate_bound(base_d,bound,bound,bound,bound)[bound:-1*bound,bound:-1*bound]
+        
+        img_d =self.gradiant(im_second,5)
+
+        best_match=[]
+        score=-1
+        pos=[0,0]
+        
+        
+        for x in range(bound*2-1):
+            for y in range(bound*2-1):
+                tmp = self.genarate_bound(img_d,x,bound*2-x,y,bound*2-y)[bound:-1*bound,bound:-1*bound]
+                score_tmp = np.sum(np.absolute(base_image-tmp))
+                if (((score_tmp)<score) or score<0):
+                    #self.save_image(np.absolute((base_image-tmp)),'./best_match.jpg')
+                    score=score_tmp
+                    pos=[x,y]
+                    best_match=self.genarate_bound(im_second,x,bound*2-x,y,bound*2-y)
+
+        print('Calculated best shift is:'),
+        print(pos)
+        return best_match[bound:-1*bound,bound:-1*bound]
+    
+    def gradiant(self,img,order):
+        im1=self.genarate_bound(img,0,order,0,0)
+        im2=self.genarate_bound(img,order,0,0,0)
+        dif = np.absolute(im1-im2)
+        return dif
+    
+    def get_stabilized_imageset(self,imgs,order):
+        ret_imgs=[]
+        for img in imgs:
+            ret_imgs.append(self.get_best_match(imgs[0],img,order))
+        return ret_imgs
+
+App = MovingObjectRemover()
+images= App.open_image_set("../pond")
+stab_images=App.get_stabilized_imageset(images,5)
+App.compute_and_save(stab_images,'./Stabilized_output.jpg')
+
